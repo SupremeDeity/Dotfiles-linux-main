04fb4e998c8b0f299ae758309809745dd31a5ffe
Mohsin Sheikh <sheikhmohsin706@gmail.com>
2021-03-14 00:31:40 +0500

    Weekly change commit

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,6 +9,7 @@ export PATH="/home/mohsin/.cargo/bin/:/home/mohsin/.local/bin/:/usr/local/bin/co
 
 export SUDO_EDITOR="/usr/bin/nvim"
 
+# For non-kitty terminals, import colorscheme
 wal -r && clear
 
 alias tb="nc termbin.com 9999"
